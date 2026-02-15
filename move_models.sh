05b11b42441360a56a89bb8e2ee0108334807fef
thecreare <thecreare@proton.me>
2026-02-14 23:16:25 -0500

    Strip testing game assets and format as a library

--- move_models.sh
+++
@@ -1,1 +0,0 @@
-rsync -r /home/creare/.var/app/org.vinegarhq.Vinegar/data/vinegar/prefixes/studio/drive_c/users/creare/Documents/polytoria-canyon/ models
--- process.sh
+++ process.sh
@@ -6,7 +6,5 @@ inotifywait --recursive --monitor --format "%e %w%f" \
 	--event modify,move,create,delete $watch \
 | while read changed; do
     echo $changed
-	lune run CompileModels
-	darklua process src/client/init.luau "$scripteditor_path/client_6Y20QVQNP1MA5EFP.lua"
-	darklua process src/server/init.luau "$scripteditor_path/server_Y4JCWRVDDHAOZBGJ.lua"
+	darklua process src/init.luau "$scripteditor_path/server_Y4JCWRVDDHAOZBGJ.lua"
 done
