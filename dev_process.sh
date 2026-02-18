2d1022d27a79fb29fe9364f6c0c65852a99f4bf0
thecreare <thecreare@proton.me>
2026-02-17 19:01:02 -0500

    Rework to function as a module & add user friendly API

--- dev_process.sh
+++ dev_process.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# cd src
+
 scripteditor_path='/run/user/1000/app/com.polytoria.launcher/Polytoria/Polytoria Creator/scripteditor'
-watch='lune/ models/ src/ .darklua.json5'
+watch='src/ .darklua.json5'
+
 inotifywait --recursive --monitor --format "%e %w%f" \
 	--event modify,move,create,delete $watch \
 | while read changed; do
     echo $changed
-	darklua process src/init.luau "$scripteditor_path/server_Y4JCWRVDDHAOZBGJ.lua"
+	darklua process src/init.luau "$scripteditor_path/ScriptInstance_Y5JHF2CSCTWVA2OA.lua"
 done
